cbbbcdbf76d07f4a7e467757ed2bc349462e3012
Florin9B <44284307+NinthBurn@users.noreply.github.com>
2024-03-09 21:07:11 +0200

    implemented save button(insert/update)

--- src/InputPanel.tsx
+++ src/InputPanel.tsx
@@ -1,6 +1,8 @@
 import './input_panel_styles.css';
 import { ComputerComponent } from './ComputerComponent';
 import React, {useState} from 'react';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 interface InputPanelProps{
   AppData: [ComputerComponent[], any];
@@ -20,8 +22,9 @@ export function DeleteButton(props: ButtonProps){
       });
       const onDataChange = props.AppData[1];
       onDataChange(x);
+      
   }
-  return <button onClick={() => deleteClicked(props.AppData[0], props.element_id)} className="DeleteButton">DELETE</button>;
+  return (<button onClick={() => deleteClicked(props.AppData[0], props.element_id)} className="DeleteButton">DELETE</button>);
 }
 
 export function EditButton(props: ButtonProps){
@@ -43,13 +46,48 @@ export function EditButton(props: ButtonProps){
   return <button onClick={() => editClicked(props.AppData[0], props.element_id)} className="EditButton">EDIT</button>;
 }
 
+interface SaveButtonProps{
+  AppData: [ComputerComponent[], any];
+  InputData: string[];
+}
+
+export function SaveButton(props: SaveButtonProps){
+  const saveClicked = (data: ComputerComponent[], inputData: string[]) => {
+      console.log("save clicked");
+      // id manufacturer name category price release_date quantity
+
+      let newData: ComputerComponent[];
+      const pc_part:ComputerComponent = {
+        id: Number(props.InputData[0]),
+        manufacturer: props.InputData[1],
+        productName: props.InputData[2],
+        category: props.InputData[3],
+        price: Number(props.InputData[4]),
+        releaseDate: stringToDate((props.InputData[5]), "dd/MM/yyyy", "/"),
+        quantity: Number(props.InputData[6])
+      };
+
+      const index = data.findIndex((part) => {return part.id === pc_part.id;});
+      if(index === -1){
+        newData = [...data, pc_part];
+      }else{
+        newData = data.map((x) => {return x;});
+        newData[index]= pc_part;
+      }
+       
+      const onDataChange = props.AppData[1];
+      onDataChange(newData);
+  }
+  return <button onClick={() => saveClicked(props.AppData[0], props.InputData)} className="InsertButton">SAVE</button>;
+}
+
 function InputPanel(props: InputPanelProps) {
     const [text, textChange] = useState(["", "", "", "", "", "", ""]);
     textChanged = (oldText: string[], newText: string, index: number) => {
       //console.log(newText);
       //console.log(index);
 
-      let t = oldText.map((x) => x);
+      const t = oldText.map((x) => x);
       t[index] = newText;
       textChange(t);
     }
@@ -65,11 +103,34 @@ function InputPanel(props: InputPanelProps) {
           })}
 
           <br/>
-          <button className="InsertButton">SAVE</button>
+          <SaveButton AppData={props.AppData} InputData={text}/>
         </div>
     );
 }
 
 let textChanged: (oldText: string[], newText: string, index: number) => void;
+
+/*
+https://stackoverflow.com/questions/5619202/parsing-a-string-to-a-date-in-javascript
+thanks Kaseem
+*/
+
+function stringToDate(_date: string, _format:string ,_delimiter: string)
+{
+  var formatLowerCase=_format.toLowerCase();
+  var formatItems=formatLowerCase.split(_delimiter);
+  var dateItems=_date.split(_delimiter);
+  var monthIndex=formatItems.indexOf("mm");
+  var dayIndex=formatItems.indexOf("dd");
+  var yearIndex=formatItems.indexOf("yyyy");
+  var month=parseInt(dateItems[monthIndex]);
+  month-=1;
+  var formatedDate = new Date(parseInt(dateItems[yearIndex]),month,parseInt(dateItems[dayIndex]));
+  return formatedDate;
+}
+
+stringToDate("17/9/2014","dd/MM/yyyy","/");
+stringToDate("9/17/2014","mm/dd/yyyy","/")
+stringToDate("9-17-2014","mm-dd-yyyy","-")
 
 export default InputPanel;
